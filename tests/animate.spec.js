abf4e3b00594415e12a6002d31dc75815c672ef0
Tim Wright <tim.wright@10up.com>
2020-03-25 09:30:48 -0400

    functional test working

--- tests/animate.spec.js
+++ tests/animate.spec.js
@@ -1,4 +1,5 @@
 import puppeteer from 'puppeteer';
+import assert from 'assert';
 
 const APP = 'https://10up.github.io/component-animate/demo/';
 const width = 1440;
@@ -41,7 +42,28 @@ describe( 'Accessibility Tests', () => {
 		// Visit the page in headless Chrome
 		await page.goto( APP );
 
+		const animation = 'fade-out'
+		const duration = 'duration-500'
 
+		// Fill and submit login form
+		await page.evaluate( ( animation, duration ) => {
+
+			document.querySelector( '#js-control' ).value = animation;
+			document.querySelector( '#js-speed' ).value = duration;
+			document.querySelector( '#js-animation-form [type="submit"]' ).click();
+
+		}, animation, duration );
+
+		const animationDuration = await page.evaluate(() => {
+
+			const demoBlock = document.getElementById( 'js-demo-block' );
+			const demoBlockStyles = window.getComputedStyle( demoBlock );
+			const animationDemoBlockDuration = demoBlockStyles.getPropertyValue( 'animation-duration' );
+			return animationDemoBlockDuration;
+
+		});
+
+		assert.equal( animationDuration, '0.001s' );
 
 	} );
 
