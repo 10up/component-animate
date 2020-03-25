75dff41266f0b74e9d5f067cce99211c1066fca4
Tim Wright <tim.wright@10up.com>
2020-03-25 11:15:37 -0400

    added a11y tests

--- tests/animate.spec.js
+++ tests/animate.spec.js
@@ -8,10 +8,39 @@ const height = 860;
 let page;
 let browser;
 
+const animations = [
+	'fade-in',
+	'fade-out',
+	'rotate-90',
+	'rotate-180',
+	'rotate-270',
+	'rotate-360',
+	'scale-down-large',
+	'scale-down-small',
+	'scale-down',
+	'scale-up-large',
+	'scale-up-small',
+	'scale-up',
+	'shake-horizontal-large',
+	'shake-horizontal-small',
+	'shake-horizontal',
+	'shake-vertical-large',
+	'shake-vertical-small',
+	'shake-vertical',
+	'slide-in-down',
+	'slide-in-left',
+	'slide-in-right',
+	'slide-in-up',
+	'slide-out-down',
+	'slide-out-left',
+	'slide-out-right',
+	'slide-out-up'
+];
+
 beforeAll( async () => {
 
 	browser = await puppeteer.launch( {
-		headless: false,
+		headless: true,
 	} );
 
 	page = await browser.newPage();
@@ -33,39 +62,38 @@ beforeEach( async () => {
 		}
 	] );
 
+	// Visit the page in headless Chrome
+	await page.goto( APP );
+
 } );
 
 describe( 'Accessibility Tests', () => {
 
-	test( 'Test Reduced Motion for: Fade', async () => {
+	for (const animation of animations) {
 
-		// Visit the page in headless Chrome
-		await page.goto( APP );
+		test( 'Test Reduced Motion for: ' + animation , async () => {
+			await page.evaluate( ( animation ) => {
 
-		const animation = 'fade-out'
-		const duration = 'duration-500'
+				document.querySelector( '#js-control' ).value = animation;
+				document.querySelector( '#js-animation-form [type="submit"]' ).click();
 
-		// Fill and submit login form
-		await page.evaluate( ( animation, duration ) => {
+			}, animation );
 
-			document.querySelector( '#js-control' ).value = animation;
-			document.querySelector( '#js-speed' ).value = duration;
-			document.querySelector( '#js-animation-form [type="submit"]' ).click();
+			const animationDuration = await page.evaluate((animation) => {
 
-		}, animation, duration );
+				const demoBlock = document.getElementById( 'js-demo-block' );
+				const demoBlockStyles = window.getComputedStyle( demoBlock );
+				const animationDemoBlockDuration = demoBlockStyles.getPropertyValue( 'animation-duration' );
 
-		const animationDuration = await page.evaluate(() => {
+				return animationDemoBlockDuration;
 
-			const demoBlock = document.getElementById( 'js-demo-block' );
-			const demoBlockStyles = window.getComputedStyle( demoBlock );
-			const animationDemoBlockDuration = demoBlockStyles.getPropertyValue( 'animation-duration' );
-			return animationDemoBlockDuration;
+			});
 
-		});
+			assert.equal( animationDuration, '0.001s' );
 
-		assert.equal( animationDuration, '0.001s' );
+		} );
 
-	} );
+	} // for()
 
 } );
 
